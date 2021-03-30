6e472387ab72c5cbdc6307f5184bddb158e4f157
Stuart Fyfe <stuart.fyfe@setl.io>
2021-03-30 09:18:03 +0100

    Fixup

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 addEventListener("fetch", async (event) => {
-  const quote = fetch("https://quotes.rest/qod?language=en").then(r => r.json());
+  const quote = await fetch("https://quotes.rest/qod?language=en").then(r => r.json());
   const response = new Response(quote.contents.quotes[0].quote, {
     headers: { "content-type": "text/plain" },
   });
