05470b4756bae00f01b45f1327be2894cf127e67
Stuart Fyfe <stuart.fyfe@setl.io>
2021-03-30 09:24:56 +0100

    Add title to page

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 addEventListener("fetch", async (event) => {
   const quote = await fetch("https://quotes.rest/qod?language=en").then(r => r.json());
-  const response = new Response(quote.contents.quotes[0].quote, {
-    headers: { "content-type": "text/plain" },
+  const response = new Response(`<h1>Quote of the day:</h1><p>${quote.contents.quotes[0].quote}</p>`, {
+    headers: { "content-type": "text/html; charset=utf8" },
   });
   event.respondWith(response);
 });
